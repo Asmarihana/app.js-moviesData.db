5d80998eec44ad151b787dfa612dc7143fa8ec0c
Asmarihana <asmashaik739@gamil.com>
2023-08-17 13:09:32 +0000

    Given two files  and a database file  consisting of two tables  and .

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ initializationAndServer();
 
 ///API1
 app.get("/states/", async (request, response) => {
-  const getQuery = `SELECT * FROM state`;
+  const getQuery = `
+  SELECT *
+ FROM state
+ ORDER BY state_id;`;
   const method = (object) => {
     return {
       stateId: object.state_id,
@@ -37,7 +40,10 @@ app.get("/states/", async (request, response) => {
     };
   };
   const getQueryState = await db.all(getQuery);
-  response.send(getQueryState.map((each) => method(each)));
+  const result = getQueryState.map((each) => {
+    return method(each);
+  });
+  response.send(result);
 });
 ///API2
 app.get("/states/:stateId/", async (request, response) => {
@@ -58,8 +64,84 @@ app.get("/states/:stateId/", async (request, response) => {
 app.post("/districts/", async (request, response) => {
   const { districtName, stateId, cases, cured, active, deaths } = request.body;
   const postQuery = `INSERT INTO 
-    district(district_id,district_name,state_id,cases,cured,active,deaths)
-    VALUES('${districtName}','${stateId}','${cases}','${cured}','${active}',${deaths}')`;
+    district(district_name,state_id,cases,cured,active,deaths)
+    VALUES('${districtName}','${stateId}','${cases}','${cured}','${active}','${deaths}')`;
   const postQueryMethod = await db.run(postQuery);
   response.send("District Successfully Added");
 });
+///API4
+app.get("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const getQuery = `
+  SELECT * 
+  FROM district
+    WHERE district_id=${districtId}`;
+  const method = (obj) => {
+    return {
+      districtId: obj.district_id,
+      districtName: obj.district_name,
+      stateId: obj.state_id,
+      cases: obj.cases,
+      cured: obj.cured,
+      active: obj.active,
+      deaths: obj.deaths,
+    };
+  };
+  const getQueryDist = await db.get(getQuery);
+  response.send(method(getQueryDist));
+});
+
+///API5
+app.delete("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const deleteQuery = `DELETE FROM district
+    WHERE district_id=${districtId}`;
+  await db.run(deleteQuery);
+  response.send("District Removed");
+});
+///API6
+app.put("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const updateQuery = `UPDATE district
+    SET 
+   district_name='${districtName}',
+   state_id='${stateId}',
+   cases='${cases}',
+   cured='${cured}',
+   active='${active}',
+    deaths='${deaths}';
+    WHERE district_id=${districtId};`;
+  await db.run(updateQuery);
+  response.send("District Details Updated");
+});
+///API7
+app.get("/states/:stateId/stats/", async (request, response) => {
+  const { stateId } = request.params;
+  const getQuery = `SELECT sum(cases) AS cases,
+  sum(cured) AS cured,
+  sum(active) AS  active,
+  sum(deaths) AS deaths
+  FROM district
+ WHERE state_id=${stateId}; `;
+  const method = (obj) => {
+    return {
+      totalCases: obj.cases,
+      totalCured: obj.cured,
+      totalActive: obj.active,
+      totalDeaths: obj.deaths,
+    };
+  };
+  const getTotal = await db.get(getQuery);
+  response.send(method(getTotal));
+});
+///API8
+app.get("/districts/:districtId/details/", async (request, response) => {
+  const { districtId } = request.params;
+  const getQuery = `SELECT state_name
+  FROM state JOIN district ON state.state_id=district.state_id
+  WHERE district.district_id=${districtId}`;
+  const getResponse = await db.get(getQuery);
+  response.send({ stateName: getResponse.state_name });
+});
+module.exports = app;
